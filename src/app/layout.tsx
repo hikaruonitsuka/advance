60ad59629551a64af590fae8179805d7a72c6981
hikaruonitsuka <linlin.work071109@gmail.com>
2024-06-08 15:03:29 +0900

    feat: ✨ ログイン前ヘッダーの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body className="text-sm sm:text-base">{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from '@/components/common/Header';
+
 export default function Home() {
   return (
     <main>
-      <p className="text-center">テスト</p>
+      <Header />
     </main>
   );
 }
