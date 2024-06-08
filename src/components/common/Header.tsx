83591cbaa283fc1d59dfa98fdc7248750203e94a
hikaruonitsuka <linlin.work071109@gmail.com>
2024-06-08 15:34:22 +0900

    style: 💄 ヘッダーのスタイルを一部修正

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="mx-auto flex max-w-7xl items-center justify-between p-4">
+    <header className="mx-auto flex w-full max-w-7xl items-center justify-between p-4">
       <Link href="/">Logo</Link>
       <div className="flex items-center gap-x-4">
         <button>ログイン</button>
