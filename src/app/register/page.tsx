59b9154daed4ee4f02713dcb4e15a867e6ef6fb6
hikaruonitsuka <linlin.work071109@gmail.com>
2024-06-11 22:48:01 +0900

    fix: 🐛 遷移先を/loginから/registerに修正

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@ import Inner from '@/components/common/Inner';
 import PageFv from '@/components/common/PageFv';
 import Typography from '@/components/common/Typography';
 
-export default function login() {
+export default function Register() {
   return (
     <>
       <PageFv />
@@ -13,6 +13,12 @@ export default function login() {
           </Typography>
           <Typography className="mt-6" component={'p'} variant={'p'}>
             テキストテキストテキストテキストテキストテキストテキスト
+          </Typography>
+          <div className="mt-10">
+            <button className="w-full rounded-lg border border-gray-300 px-4 py-3 text-sm font-bold">GitHub</button>
+          </div>
+          <Typography className="mt-4 text-sm" component={'p'} variant={'p'}>
+            利用規約とプライバシーポリシーに同意したことになります。
           </Typography>
         </Inner>
       </div>
