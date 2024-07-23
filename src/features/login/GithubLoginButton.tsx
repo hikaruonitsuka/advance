5d646f44aa4e96a87a31d1c5b329da25ed19f653
hikaruonitsuka <linlin.h071109@gmail.com>
2024-07-23 14:44:32 +0900

    feat: ✨ ログインボタンを実装

--- src/features/login/GithubLoginButton.tsx
+++ src/features/login/GithubLoginButton.tsx
@@ -5,18 +5,14 @@ import { FaGithub } from 'react-icons/fa6';
 import { useAuth } from '@/hooks/useAuth';
 
 export default function GithubLoginButton() {
-  const { signInWithGithub, loading, error } = useAuth();
+  const { signInWithGithub } = useAuth();
   return (
-    <>
-      {error && <p className="text-red-500">{error}</p>}
-      <button
-        className="mx-auto flex w-full max-w-md items-center justify-center gap-x-2 rounded-lg border border-gray-300 px-4 py-3 text-sm font-bold transition-opacity hover:opacity-60 sm:text-base"
-        disabled={loading}
-        onClick={() => void signInWithGithub()}
-      >
-        <FaGithub className="sm:size-5" />
-        GitHub
-      </button>
-    </>
+    <button
+      className="mx-auto flex w-full max-w-md items-center justify-center gap-x-2 rounded-lg border border-gray-300 px-4 py-3 text-sm font-bold transition-opacity hover:opacity-60 sm:text-base"
+      onClick={() => void signInWithGithub()}
+    >
+      <FaGithub className="sm:size-5" />
+      GitHub
+    </button>
   );
 }
