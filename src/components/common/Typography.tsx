c3f2df0bff222210108269230c39f80cfcec730f
hikaruonitsuka <linlin.h071109@gmail.com>
2024-07-24 15:53:48 +0900

    refactor: ♻️ Typographyコンポーネントのvariantをオプショナルに修正

--- src/components/common/Typography.tsx
+++ src/components/common/Typography.tsx
@@ -6,7 +6,7 @@ import type { Simplify } from 'type-fest';
 
 type Props = Simplify<{
   component: React.ElementType;
-  variant: 'h1' | 'h2' | 'h3' | 'h4' | 'p';
+  variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'p';
   children: React.ReactNode;
   className?: ClassNameValue;
 }>;
@@ -14,16 +14,22 @@ type Props = Simplify<{
 const Typography = ({ component, variant, className, children, ...props }: Props) => {
   const Component = component;
 
-  const fontStyle = match(variant)
-    .with('h1', () => clsx('text-3xl'))
-    .with('h2', () => clsx('text-2xl', 'font-bold'))
-    .with('h3', () => clsx('text-xl'))
-    .with('h4', () => clsx('text-lg'))
-    .with('p', () => clsx('text-base'))
-    .exhaustive();
+  // variant未指定だとstyleを適用しない
+  const fontStyle = variant
+    ? match(variant)
+        .with('h1', () => clsx('text-3xl'))
+        .with('h2', () => clsx('text-2xl', 'font-bold'))
+        .with('h3', () => clsx('text-xl'))
+        .with('h4', () => clsx('text-lg'))
+        .with('p', () => clsx('text-sm', 'sm:text-base'))
+        .exhaustive()
+    : '';
+
+  // variant, className共に未指定だとclassを出力しない
+  const mergedClassName = variant || className ? twMerge(fontStyle, className) : undefined;
 
   return (
-    <Component className={twMerge(fontStyle, className)} {...props}>
+    <Component className={mergedClassName} {...props}>
       {children}
     </Component>
   );
