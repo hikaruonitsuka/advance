0c8348d6b56d192ef4af884f4ce5148f1491f3d5
hikaruonitsuka <linlin.h071109@gmail.com>
2024-09-27 21:12:15 +0900

    refactor: ♻️ Prismaのテーブル定義とマイグレーションファイルを一部修正

--- prisma/migrations/20240926125416_init/migration.sql
+++ prisma/migrations/20240926125416_init/migration.sql
@@ -1,19 +1,22 @@
+-- CreateEnum
+CREATE TYPE "Genders" AS ENUM ('male', 'female');
+
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "Users" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Profile" (
+CREATE TABLE "Profiles" (
     "id" TEXT NOT NULL,
     "avatar_image_url" TEXT,
     "profile_name" TEXT NOT NULL,
-    "gender" VARCHAR(10) NOT NULL,
+    "gender" "Genders" NOT NULL,
     "learning_category" TEXT,
     "self_introduction" TEXT,
     "is_private" BOOLEAN NOT NULL DEFAULT false,
@@ -21,14 +24,14 @@ CREATE TABLE "Profile" (
     "updated_at" TIMESTAMP(3) NOT NULL,
     "user_id" TEXT NOT NULL,
 
-    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Profiles_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Profile_user_id_key" ON "Profile"("user_id");
+CREATE UNIQUE INDEX "Profiles_user_id_key" ON "Profiles"("user_id");
 
 -- AddForeignKey
-ALTER TABLE "Profile" ADD CONSTRAINT "Profile_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Profiles" ADD CONSTRAINT "Profiles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
