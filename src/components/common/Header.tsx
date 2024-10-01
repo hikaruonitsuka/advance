82cab25ba7448e32ed6a8fa75c8b977f3e307d1b
hikaruonitsuka <linlin.h071109@gmail.com>
2024-10-01 22:58:51 +0900

    refactor: ♻️ Headerコンポーネントを一部修正

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,16 +6,15 @@ import { useAuth } from '@/hooks/useAuth';
 
 export default function Header() {
   const { session, signInWithGithub, signOut } = useAuth();
-  console.log(session);
 
   return (
     <header className="mx-auto flex w-full max-w-7xl items-center justify-between p-4">
       <Link href="/">Logo</Link>
       {session ? (
-        <button onClick={() => void signOut()}>ログアウト</button>
+        <button onClick={() => signOut()}>ログアウト</button>
       ) : (
         <div className="flex items-center gap-x-4">
-          <button onClick={() => void signInWithGithub()}>ログイン</button>
+          <button onClick={() => signInWithGithub()}>ログイン</button>
           <Link href="/register">新規登録</Link>
         </div>
       )}
