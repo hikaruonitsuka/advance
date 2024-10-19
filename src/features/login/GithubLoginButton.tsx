3699a5365aebe9c8da00af00666ce9a29f4e2c9a
hikaruonitsuka <linlin.h071109@gmail.com>
2024-10-19 20:06:51 +0900

    feat: ✨ 新規登録ページのGitHubで新規登録ボタンに認証機能を設定

--- src/features/login/GithubLoginButton.tsx
+++ src/features/login/GithubLoginButton.tsx
@@ -1,18 +1,14 @@
-'use client';
-
 import { FaGithub } from 'react-icons/fa6';
 
-import { useAuth } from '@/hooks/useAuth';
+import { signInWithGithub } from '@/app/data/auth';
 
 export default function GithubLoginButton() {
-  const { signInWithGithub } = useAuth();
   return (
-    <button
-      className="mx-auto flex w-full max-w-md items-center justify-center gap-x-2 rounded-lg border border-gray-300 px-4 py-3 text-sm font-bold transition-opacity hover:opacity-60 sm:text-base"
-      onClick={() => void signInWithGithub()}
-    >
-      <FaGithub className="sm:size-5" />
-      GitHub
-    </button>
+    <form action={signInWithGithub}>
+      <button className="mx-auto flex w-full max-w-md items-center justify-center gap-x-2 rounded-lg border border-gray-300 px-4 py-3 text-sm font-bold transition-opacity hover:opacity-60 sm:text-base">
+        <FaGithub className="sm:size-5" />
+        GitHub
+      </button>
+    </form>
   );
 }
