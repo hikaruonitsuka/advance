ea43712c55c7f6ba9d798b75de60e5f1d1d2bbd8
hikaruonitsuka <linlin.h071109@gmail.com>
2024-10-20 14:54:17 +0900

    feat: ✨ Next.js用のミドルウェアを追加

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { type NextRequest } from 'next/server';
+
+import { updateSession } from '@/lib/supabase/middleware';
+
+/**
+ * 特定のリクエストパスを除外するためのマッチャー設定を含むミドルウェア
+ */
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
+
+/*
+ * 以下のパスを除外します:
+ * - _next/static (静的ファイル)
+ * - _next/image (画像最適化ファイル)
+ * - favicon.ico (ファビコンファイル)
+ * - .svg, .png, .jpg, .jpeg, .gif, .webp 拡張子のファイル
+ *
+ * 以下のヘッダーが存在しない場合にマッチします:
+ * https://zenn.dev/sasatech/articles/035e634d7ea8c3
+ *
+ * - next-router-prefetch
+ * - purpose: prefetch
+ */
+export const config = {
+  matcher: [
+    {
+      source: '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+  ],
+};
